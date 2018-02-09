b6103a00b4fa786511c4024d6b11514446b0bbda
m-ali-ubit <usermalikhan@gmail.com>
2018-02-09 18:42:56 +0500

    Print string, integer, list, tuple and dictionary using variables

---
+++ PrintUsingVariables.py
@@ -0,0 +1,27 @@
+
+# Printing using variables
+
+hello = 'hello'     # we can also use single inverted commas.
+world = "world"
+
+integer = 10
+
+decimal = 5.5
+
+lst = [1, 2, 3, 'a', 'b', 'c', 4.5, 5.6, 6.7]
+
+tupl = (1, 2, 3, 'a', 'b', 'c', 4.5, 5.6, 6.7)
+
+dictionary = {'a': 1, 'b': 2, 'c': 3, 'd': 4.5, 'e': 5.6, 'f': 6.7}  # { key : value }
+
+print(hello + ' ' + world)    # here + is used for concatenation.
+
+print(integer)
+
+print(decimal)
+
+print(lst)
+
+print(tupl)
+
+print(dictionary)
