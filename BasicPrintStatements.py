979d3abec76691b35b436ae2e52d2bc702522fcc
m-ali-ubit <usermalikhan@gmail.com>
2018-02-09 18:30:21 +0500

    Basic prints statements ; how to print string, integer, list, tuple and dictionary.

---
+++ BasicPrintStatements.py
@@ -0,0 +1,27 @@
+
+# Simple print statement.
+
+# string
+print("Hello World")
+
+# Do not use inverted commas to print integer and float.
+# Integer
+print(10)       # decimal ; base 10
+print(0b111)    # 0b represents binary number
+print(0o37)     # 0o represents octal number
+print(0xA0F)    # 0x represents hexadecimal number
+
+# Float
+print(5.5)
+
+# complex number ; a + bj, here a and b are integers or floats
+print(2+8j)      # j is imaginary number representing sq root of -1
+
+# List []
+print([1, 2, 3, 'a', 'b', 'c', 4.5, 5.6, 6.7])          # [1, 2, 3, 'a', 'b', 'c', 4.5, 5.6, 6.7]
+
+# Tuple    ()
+print((1, 2, 3, 'a', 'b', 'c', 4.5, 5.6, 6.7))          # (1, 2, 3, 'a', 'b', 'c', 4.5, 5.6, 6.7)
+
+# Dictionary   {}
+print({'a': 1, 'b': 2, 'c': 3, 'd': 4.5, 'e': 5.6})     # {'a': 1, 'b': 2, 'c': 3, 'd': 4.5, 'e': 5.6}
