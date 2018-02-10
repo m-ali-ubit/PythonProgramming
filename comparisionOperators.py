4ba5a659d49ffa77f20c49072dccf78db7514afa
m-ali-ubit <usermalikhan@gmail.com>
2018-02-10 12:29:54 +0500

    use of basic comparision operators

---
+++ comparisionOperators.py
@@ -0,0 +1,15 @@
+
+x = 10
+y = 15
+
+print(x == y)   # returns TRUE if both are same else FALSE
+
+print(x != y)   # returns TRUE if both are not same else FALSE
+
+print(x > y)   # returns TRUE if x is greater than y else FALSE
+
+print(x < y)   # returns TRUE if x is less than y else FALSE
+
+print(x >= y)   # returns TRUE if x is greater than or equal to y else FALSE
+
+print(x <= y)   # returns TRUE if is x less than or equal to y else FALSE
