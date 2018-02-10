8f0870a9335be269354a56290971a73c70763f93
m-ali-ubit <usermalikhan@gmail.com>
2018-02-10 12:33:24 +0500

    use of basic logical operators

---
+++ logicalOperators.py
@@ -0,0 +1,10 @@
+
+x = True
+y = False
+
+print(x and y)  # returns TRUE if all values are true
+
+print(x or y)   # returns TRUE if any value is true
+
+print(not x)    # returns opposite of true  ;   False
+print(not y)    # returns opposite of false ;   True
