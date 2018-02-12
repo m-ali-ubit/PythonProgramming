f108d77301e203e05b7aea34591384e023351d9f
m-ali-ubit <usermalikhan@gmail.com>
2018-02-12 19:09:30 +0500

    decision making ; use of if, elif and else

---
+++ decisionMaking.py
@@ -0,0 +1,21 @@
+
+# if else are conditional statements used for decision making
+
+x = 10
+
+if x == 10:
+    print('condition is true and value of x is', x)
+else:
+    print('condition false')
+
+
+# if: elif: else:
+
+y = 10
+
+if y == 30:
+    print('if condition is true, value of y is 30')
+elif y == 20:
+    print('elif condition is true, value of y is 20')
+else:
+    print('value of y is other than 20 and 30, value of y is', y)
